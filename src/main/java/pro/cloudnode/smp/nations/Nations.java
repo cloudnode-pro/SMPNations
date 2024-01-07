8e490439fb5ce5848e34e9c60ad6f7602f58c4d6
km127pl <km127pl@gmail.com>
2024-01-07 22:32:49 +0100

    optimise imports

--- src/main/java/pro/cloudnode/smp/nations/Nations.java
+++ src/main/java/pro/cloudnode/smp/nations/Nations.java
@@ -1,18 +1,13 @@
 package pro.cloudnode.smp.nations;
 
-import com.loohp.interactivechat.api.InteractiveChatAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import pro.cloudnode.smp.nations.commands.NationsCommand;
 import pro.cloudnode.smp.nations.listeners.ChatMessageListener;
 import pro.cloudnode.smp.nations.locale.Messages;
-import pro.cloudnode.smp.nations.util.Nation;
 import pro.cloudnode.smp.nations.util.NationManager;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public final class Nations extends JavaPlugin {
