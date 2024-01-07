a3963cc03ec1ac78a42ec6ffebce72a0ccf32fe1
km127pl <km127pl@gmail.com>
2024-01-07 04:34:53 +0100

    Initial commit

---
+++ src/main/java/pro/cloudnode/smp/countries/Countries.java
@@ -0,0 +1,16 @@
+package pro.cloudnode.smp.countries;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Countries extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
